5768140968b1487bfdfeff1b3def534266b7e500
= <=>
2024-01-12 13:47:44 -0600

    add basic setup of foundry and voting system contract

---
+++ src/VotingSystem.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+
+contract VotingSystem {
+
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+    mapping(address => bool) public voters;
+    mapping(uint => Candidate) public candidates;
+
+    uint public candidatesCount;
+
+    constructor() {
+        addCandidate("Alice");
+        addCandidate("Bob");
+    }
+
+    function addCandidate(string memory _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote(uint _candidateId) public {
+        require(!voters[msg.sender], "You have already voted.");
+        require(_candidateId > 0 && _candidateId <= candidatesCount, "Invalid candidate to vote for.");
+        voters[msg.sender] = true;
+        candidates[_candidateId].voteCount ++;
+        emit votedEvent(_candidateId);
+    }
+    event votedEvent (
+        uint indexed _candidateId
+    );
+}
