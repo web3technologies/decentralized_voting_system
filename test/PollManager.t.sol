84949b8ad8ff40e74a0de3f6f797467f3800312a
= <=>
2024-01-15 13:44:57 -0600

    add more tests

--- test/PollManager.t.sol
+++ test/PollManager.t.sol
@@ -10,10 +10,16 @@ import { PollInstance, PollManager } from "../src/PollManager.sol";
 contract PollManagerTest is Test {
 
     PollManager pollManager;
+    
+    string[] pollOptions = ["blue", "green", "red"];
+
+    address USER = makeAddr("user");
+
 
     function setUp() external {
         DeployPollManager deployPollManager = new DeployPollManager();
         pollManager = deployPollManager.run();
+        vm.deal(USER, 10 ether);
     }
 
     function testMinimumDollarIsFive() public {
@@ -27,6 +33,24 @@ contract PollManagerTest is Test {
     function testPriceFeedVersionIsAccurate() public {
         uint256 version = pollManager.getVersion();
         assertEq(version, 4);
+    }
+
+    function testCreatePollWithoutEnoughEth() public {
+        vm.expectRevert();
+        pollManager.createNewPollingInstance(
+            "What is your favorite color",
+            1,
+            pollOptions
+        );
+    }
+
+    function testCreatePoll() public {
+        vm.prank(USER);
+        pollManager.createNewPollingInstance{value: .01 ether}(
+            "What is your favorite color",
+            1,
+            pollOptions
+        );
     }
 
 }
